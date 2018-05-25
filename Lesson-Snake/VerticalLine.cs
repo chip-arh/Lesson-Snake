c579ddebf42262d8c9d5db2190fea567b1006b84
Chip <chip-arh@rambler.ru>
2018-05-26 02:10:23 +0300

    Vertical line

---
+++ Lesson-Snake/VerticalLine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_Snake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int x, int yTop, int yButtom, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yTop; y <= yButtom; y++)
+            {
+                pList.Add(new Point(x, y, sym));
+            }
+        }
+        public void Draw()
+        {
+            foreach (var p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
